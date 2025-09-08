527e400d847abaf28d01b28702286280bf99a9fd
jjunis <owj1106@gmail.com>
2025-09-08 13:16:48 +0900

    [Feat] 기본 통신 연결 테스트

---
+++ Unity/Assets/Scr/basicMain.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class basicMain : MonoBehaviour
+{
+    public Button Hello;
+    public string host;                 //IP 주소 (로컬에서 127.0.0.1)
+    public int port;                    //포트 주소 (3000번으로 express 동작)
+    public string roule;                //about 주소
+
+    public void Start()
+    {
+        this.Hello.onClick.AddListener(() =>
+        {
+            var url = string.Format("{0}:{1}/{2}", host, port, roule);              //url 주소를 완성한다. (ex: 127.0.0.1:3000/about)
+            Debug.Log(url);
+
+            StartCoroutine(this.GetBasic(url, (raw) =>
+            {
+                Debug.LogFormat("{0}", raw);
+            }));
+        });
+    }
+    private IEnumerator GetBasic(string url , System.Action<string> callback)
+    {
+        var webRequest = UnityWebRequest.Get(url);
+        yield return webRequest.SendWebRequest();
+
+        if(webRequest.result == UnityWebRequest.Result.ConnectionError
+            || webRequest.result == UnityWebRequest.Result.ProtocolError)
+        {
+            Debug.Log("네트워크 환경이 좋지 않아서 통신 불가");
+        }
+        else
+        {
+            callback(webRequest.downloadHandler.text);
+        }
+    }
+}
---
+++ Unity/Assets/Scr/basicProtoclos.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basicProtoclos
+{
+    public class Packets
+    {
+        public class res
+        {
+            public string message;
+        }
+    }
+}
